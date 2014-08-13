36557930b17ef1760364ad87c101133ae8d08913
charlie <charlie@yandex-team.ru>
2014-08-13 16:47:13 +0400

    fix ListablePropertyPlaceholderConfigurerTest

--- camelot-core/src/test/java/ru/yandex/qatools/camelot/spring/ListablePropertyPlaceholderConfigurerTest.java
+++ camelot-core/src/test/java/ru/yandex/qatools/camelot/spring/ListablePropertyPlaceholderConfigurerTest.java
@@ -1,23 +1,24 @@
 package ru.yandex.qatools.camelot.spring;
 
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
 import org.junit.Test;
-import org.springframework.core.io.Resource;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.io.IOException;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_CLASS;
 import static ru.yandex.qatools.camelot.spring.ListablePropertyPlaceholderConfigurer.getProperties;
-import static ru.yandex.qatools.camelot.util.ReflectUtil.resolveResourcesFromPattern;
 
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:camelot-core-context.xml", "classpath*:test-camelot-core-context.xml"})
+@DirtiesContext(classMode = AFTER_CLASS)
 public class ListablePropertyPlaceholderConfigurerTest {
 
     @Test
     public void testPropertiesLoad() throws IOException, ClassNotFoundException {
-        ListablePropertyPlaceholderConfigurer configurer = new ListablePropertyPlaceholderConfigurer();
-        Collection<Resource> resources = resolveResourcesFromPattern("classpath:test-config.properties");
-        configurer.setLocations(resources.toArray(new Resource[resources.size()]));
-
         assertEquals("test", getProperties().getProperty("custom.plugin.stringValue"));
     }
 }
