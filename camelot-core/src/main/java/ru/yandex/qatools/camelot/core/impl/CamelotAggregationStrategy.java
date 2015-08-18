b6e94ebd2a1db13b0ce558aea2d05c05663a965a
innokenty <innokenty@yandex-team.ru>
2015-08-18 13:42:16 +0300

    fix & improve logging of resent message id

--- camelot-core/src/main/java/ru/yandex/qatools/camelot/core/impl/CamelotAggregationStrategy.java
+++ camelot-core/src/main/java/ru/yandex/qatools/camelot/core/impl/CamelotAggregationStrategy.java
@@ -43,7 +43,8 @@ public class CamelotAggregationStrategy extends FSMAggregationStrategy implement
     public void process(Exchange message) {
         final boolean resent = Boolean.parseBoolean((String) message.getIn().getHeader(MESSAGE_RESENT_HEADER));
         if (resent) {
-            logger.info("Handling previously resent message with id '{}'", message.getExchangeId());
+            logger.info("Handling previously resent message for plugin '{}' with id '{}'",
+                    context.getId(), message.getExchangeId());
         }
 
         final String key = (String) message.getIn().getHeader(CORRELATION_KEY);
@@ -131,7 +132,8 @@ public class CamelotAggregationStrategy extends FSMAggregationStrategy implement
         Exchange copy = message.copy();
         copy.getIn().setHeader(MESSAGE_RESENT_HEADER, true);
         retryProducer.send(copy);
-        logger.info("Successfully resent message with id '{}'" + message.getExchangeId());
+        logger.info("Successfully resent message for plugin '{}' with id '{}'",
+                context.getId(), copy.getExchangeId());
         message.setProperty(Exchange.ROUTE_STOP, Boolean.TRUE);
     }
 
