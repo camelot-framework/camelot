9e43b462bb1e55920c4ea419c7386d445e5533cc
Ilya Sadykov <smecsia@gmail.com>
2015-11-15 05:12:37 +0300

    Fix circular dependency in beans

--- camelot-core/src/main/java/ru/yandex/qatools/camelot/core/impl/PluginsContextIntoBeansInjector.java
+++ camelot-core/src/main/java/ru/yandex/qatools/camelot/core/impl/PluginsContextIntoBeansInjector.java
@@ -3,42 +3,55 @@ package ru.yandex.qatools.camelot.core.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.core.Ordered;
-import ru.yandex.qatools.camelot.common.ProcessingEngine;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.support.AbstractApplicationContext;
+import ru.yandex.qatools.camelot.common.PluginsService;
 
 /**
  * @author Ilya Sadykov (mailto: smecsia@yandex-team.ru)
  */
 @SuppressWarnings("unchecked")
-public class PluginsContextIntoBeansInjector implements BeanPostProcessor, Ordered {
-    public static final int PRECEDENCE = Ordered.LOWEST_PRECEDENCE - 50000;
+public class PluginsContextIntoBeansInjector implements ApplicationListener, ApplicationContextAware {
     protected final static Logger LOGGER = LoggerFactory.getLogger(PluginsContextIntoBeansInjector.class);
 
-    @Autowired
-    private ProcessingEngine processingEngine;
+    private ApplicationContext context;
     private SpringContextInjector springInjector = new SpringContextInjector();
+    private final PluginsService pluginsService;
 
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        return performInjection(bean);
-    }
-
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return performInjection(bean);
-    }
-
-    @Override
-    public int getOrder() {
-        return PRECEDENCE;
+    public PluginsContextIntoBeansInjector(PluginsService pluginsService) {
+        this.pluginsService = pluginsService;
     }
 
     private Object performInjection(Object bean) {
-        if (processingEngine != null) {
-            springInjector.inject(bean, processingEngine, null);
+        if (pluginsService != null) {
+            springInjector.inject(bean, pluginsService, null);
         }
         return bean;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+    }
+
+    /**
+     * Inject plugin context into all Spring beans after context is initialized completely
+     */
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ContextRefreshedEvent) {
+            ConfigurableListableBeanFactory clbf = ((AbstractApplicationContext) context).getBeanFactory();
+            for (String name : context.getBeanDefinitionNames()) {
+                final Object singleton = clbf.getSingleton(name);
+                if(singleton != null) {
+                    performInjection(singleton);
+                }
+            }
+        }
     }
 }
