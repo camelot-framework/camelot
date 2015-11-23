c19b470b68a994246dd58c06871b27019be6077a
Ilya Sadykov <smecsia@gmail.com>
2015-11-23 17:49:56 +0300

    Small tests

--- camelot-ext/camelot-rabbitmq/src/main/java/ru/yandex/qatools/camelot/rabbitmq/RabbitmqInterimProcessor.java
+++ camelot-ext/camelot-rabbitmq/src/main/java/ru/yandex/qatools/camelot/rabbitmq/RabbitmqInterimProcessor.java
@@ -3,8 +3,6 @@ package ru.yandex.qatools.camelot.rabbitmq;
 import org.apache.camel.Exchange;
 import ru.yandex.qatools.camelot.common.InterimProcessor;
 
-import java.util.HashSet;
-
 /**
  * @author Ilya Sadykov
  */
@@ -12,9 +10,9 @@ public class RabbitmqInterimProcessor implements InterimProcessor {
 
     @Override
     public void process(Exchange exchange) throws Exception { //NOSONAR
-        // Cleaning up the kafka headers as they must be set by kafka producer from scratch
-        new HashSet<>(exchange.getIn().getHeaders().keySet()).stream()
-               .filter(header -> header.startsWith("ru/yandex/qatools/camelot/rabbitmq")).forEach(
-                header -> exchange.getIn().removeHeader(header));
+        // Cleaning up the rabbitmq headers as they must be set by rabbitmq producer from scratch
+        exchange.getIn().getHeaders().keySet().stream()
+                .filter(header -> header.startsWith("rabbitmq"))
+                .forEach(header -> exchange.getIn().removeHeader(header));
     }
 }
---
+++ camelot-ext/camelot-rabbitmq/src/test/java/ru/yandex/qatools/camelot/rabbitmq/RabbitmqPluginUriBuilderTest.java
@@ -0,0 +1,26 @@
+package ru.yandex.qatools.camelot.rabbitmq;
+
+import org.junit.Test;
+import ru.yandex.qatools.camelot.config.Plugin;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ilya Sadykov
+ */
+public class RabbitmqPluginUriBuilderTest {
+    @Test
+    public void testBuildValidUris() throws Exception {
+        RabbitmqPluginUriBuilder builder = new RabbitmqPluginUriBuilder("localhost:1,localhost:2", "");
+
+        assertThat(builder.basePluginUri(), is("rabbitmq://localhost:1/"));
+        assertThat(builder.frontendBroadcastUri(), is("rabbitmq://localhost:1/all.frontend.notify" +
+                "?queue=all.frontend.notify&addresses=localhost:1,localhost:2&exchangeType=topic"));
+        final Plugin plugin = new Plugin();
+        plugin.setBaseInputUri(builder.basePluginUri());
+        plugin.setId("plugin.id");
+        assertThat(builder.pluginUri(plugin, "", ""), is("rabbitmq://localhost:1/plugin.id" +
+                "?queue=plugin.id&addresses=localhost:1,localhost:2"));
+    }
+}
