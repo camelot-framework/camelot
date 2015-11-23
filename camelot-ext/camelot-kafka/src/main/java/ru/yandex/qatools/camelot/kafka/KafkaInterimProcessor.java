ef9d2cdbd441ae4f5dc932386eb572950f653b1b
Ilya Sadykov <smecsia@gmail.com>
2015-11-24 02:24:13 +0300

    Add rabbitmq host port

--- camelot-ext/camelot-kafka/src/main/java/ru/yandex/qatools/camelot/kafka/KafkaInterimProcessor.java
+++ camelot-ext/camelot-kafka/src/main/java/ru/yandex/qatools/camelot/kafka/KafkaInterimProcessor.java
@@ -3,6 +3,8 @@ package ru.yandex.qatools.camelot.kafka;
 import org.apache.camel.Exchange;
 import ru.yandex.qatools.camelot.common.InterimProcessor;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * @author Ilya Sadykov
  */
@@ -11,7 +13,7 @@ public class KafkaInterimProcessor implements InterimProcessor {
     @Override
     public void process(Exchange exchange) throws Exception { //NOSONAR
         // Cleaning up the kafka headers as they must be set by kafka producer from scratch
-        exchange.getIn().getHeaders().keySet().stream()
+        exchange.getIn().getHeaders().keySet().stream().collect(toList()).stream()
                 .filter(header -> header.startsWith("kafka"))
                 .forEach(header -> exchange.getIn().removeHeader(header));
     }
--- camelot-ext/camelot-rabbitmq/src/main/java/ru/yandex/qatools/camelot/rabbitmq/RabbitmqInterimProcessor.java
+++ camelot-ext/camelot-rabbitmq/src/main/java/ru/yandex/qatools/camelot/rabbitmq/RabbitmqInterimProcessor.java
@@ -3,6 +3,8 @@ package ru.yandex.qatools.camelot.rabbitmq;
 import org.apache.camel.Exchange;
 import ru.yandex.qatools.camelot.common.InterimProcessor;
 
+import static java.util.stream.Collectors.toList;
+
 /**
  * @author Ilya Sadykov
  */
@@ -11,7 +13,7 @@ public class RabbitmqInterimProcessor implements InterimProcessor {
     @Override
     public void process(Exchange exchange) throws Exception { //NOSONAR
         // Cleaning up the rabbitmq headers as they must be set by rabbitmq producer from scratch
-        exchange.getIn().getHeaders().keySet().stream()
+        exchange.getIn().getHeaders().keySet().stream().collect(toList()).stream()
                 .filter(header -> header.startsWith("rabbitmq"))
                 .forEach(header -> exchange.getIn().removeHeader(header));
     }
